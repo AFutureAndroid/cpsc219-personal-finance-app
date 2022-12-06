080801fcab0a456867e8db4e9d89173c84529ac0
Alfred JainSingh <alfred.jainsingh@ucalgary.ca>
2022-12-05 17:05:03 -0700

    Budget and initial balance updating now.

--- src/application/BudgetController.java
+++ src/application/BudgetController.java
@@ -2,28 +2,39 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class BudgetController {
 
-    @FXML
-    private TextField budgetTextfield;
-    
-    @FXML
-    private Label currentBalance;
-
-    //@FXML
-    //void getExpenses(ActionEvent EnterExpensesEvent) 
-    
-    //@FXML
-    //void getPurchaseList(ActionEvent ShowPurchaseListEvent)
+	@FXML
+	private Label bdgDisplay;
+	
+	@FXML
+	private Label expDisplay;
+	
+	@FXML
+	private Label balDisplay;
+	
+	@FXML
+	private ChoiceBox<String> expType;
+	
+	@FXML
+	private TextField budget;
+	
+	@FXML
+	private TextField note;
+	
+	@FXML
+	private TextField expAmount;
     
     @FXML
     void enterBudget(ActionEvent event) {
     	double currentBal = 0.0;
-    	double budget = Double.parseDouble(budgetTextfield.getText());
-    	currentBal = budget;
-    	currentBalance.setText(String.format("$%.2f", currentBal));
+    	double money = Double.parseDouble(budget.getText());
+    	currentBal = money;
+    	bdgDisplay.setText(String.format("$%.2f", currentBal));
+    	balDisplay.setText(String.format("$%.2f", currentBal));
     }
 }
