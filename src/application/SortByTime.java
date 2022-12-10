dc2a10a3557f01ce7f33cabf8ca4d0ff9f41ee4c
Alfred JainSingh <alfred.jainsingh@ucalgary.ca>
2022-12-09 21:45:57 -0700

    Added javadoc to SortByTime class.

--- src/application/SortByTime.java
+++ src/application/SortByTime.java
@@ -2,8 +2,12 @@ package application;
 
 import java.util.Comparator;
 
+/**
+ * Compares two expense entries based on their dates.
+ * Can be used to sort entries by date.
+ */
 public class SortByTime implements Comparator <ExpenseEntry> {
-	
+
 	@Override
 	public int compare(ExpenseEntry e1, ExpenseEntry e2) {
 		if(e1 == null || e2 == null) return 0;
@@ -19,9 +23,5 @@ public class SortByTime implements Comparator <ExpenseEntry> {
 		else if(e1.getExpDate().getYear() > e2.getExpDate().getYear()) return 1;
 		else return -1;
 	}
-		
-		
-
-	
 
 }
